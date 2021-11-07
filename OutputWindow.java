7c1332dbbdb4ab66493315114ffe07c4aedc7f7b
michaelPro89 <47365331+michaelPro89@users.noreply.github.com>
2021-11-07 14:32:27 +0100

    Update OutputWindow.java

--- OutputWindow.java
+++ OutputWindow.java
@@ -1,9 +1,15 @@
 package codebreaker;
-
-/**
- *
- * @author Michal Switala
- */
+/*
+* I have created input and output Windows applications which are a custom made java windows designed to take input from user or display some information as output to the user. 
+* They have been designed and written by using Java Swing Components like Jarea, Jlabel, Jbutton, Jframe etc. 
+* I have also used notifyAll() method to make an application wait for users input.
+*
+* - You may use this Class in your application for non commercial use if you wish but you have to keep this comment below :
+*
+* @author Michal Switala
+* Copyright © 2021 belongs solely to Michal Switala. 
+* You can reach me out on : https://github.com/michaelPro89
+*/
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import java.awt.*;
